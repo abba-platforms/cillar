b628371e2caba574cd4d0a43a1d6dda13eabb229
Afrail Inc. <simon@afrail.xyz>
2025-06-27 17:35:55 +0200

    Add utils/web3.js for MetaMask support
    Introduced a utility module to handle MetaMask connectivity using ethers.js. 
    Provides helper functions to access the Web3 provider and request a signer for executing transactions.

--- utils/web3.js
+++ utils/web3.js
@@ -1,1 +1,17 @@
+// File: utils/web3.js
 
+import { ethers } from "ethers";
+
+export function getProvider() {
+  if (typeof window.ethereum !== "undefined") {
+    return new ethers.providers.Web3Provider(window.ethereum);
+  } else {
+    throw new Error("MetaMask is not installed");
+  }
+}
+
+export async function getSigner() {
+  const provider = getProvider();
+  await provider.send("eth_requestAccounts", []);
+  return provider.getSigner();
+}
