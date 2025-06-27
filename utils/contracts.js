c7229cec96d9edd1b211d668e85630ba4dbd5125
Afrail Inc. <simon@afrail.xyz>
2025-06-27 17:40:46 +0200

    Add utils/contracts.js for loading smart contract interfaces
    Introduced utils/contracts.js to initialize and export contract instances for CillarCoin, FareRegistry, and CillarVesting using ethers.js. This module uses the signer from web3.js and references ABIs and addresses defined in constants.js. It enables seamless interaction with deployed contracts from the frontend.

--- utils/contracts.js
+++ utils/contracts.js
@@ -1,1 +1,30 @@
 
+// File: utils/contracts.js
+
+import { ethers } from "ethers";
+import { getSigner } from "./web3";
+import {
+  CILLAR_ADDRESS,
+  FARE_REGISTRY_ADDRESS,
+  VESTING_ADDRESS,
+} from "./constants";
+
+// Import ABIs (update with actual paths in your project)
+import CillarABI from "../abis/CillarCoin.json";
+import FareRegistryABI from "../abis/FareRegistry.json";
+import VestingABI from "../abis/CillarVesting.json";
+
+export async function getCillarContract() {
+  const signer = await getSigner();
+  return new ethers.Contract(CILLAR_ADDRESS, CillarABI, signer);
+}
+
+export async function getFareRegistryContract() {
+  const signer = await getSigner();
+  return new ethers.Contract(FARE_REGISTRY_ADDRESS, FareRegistryABI, signer);
+}
+
+export async function getVestingContract() {
+  const signer = await getSigner();
+  return new ethers.Contract(VESTING_ADDRESS, VestingABI, signer);
+}
